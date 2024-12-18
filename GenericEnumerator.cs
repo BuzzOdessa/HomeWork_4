f7d239e0f8957dd36d39858a4fad09c9c2be7eff
Serge <Serge.Buzz@gmail.com>
2024-12-18 12:38:04 +0200

    Вроде все

---
+++ GenericEnumerator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+
+namespace HomeWork_4;
+internal class GenericEnumerator<T> : IEnumerable<T>
+{
+    private T[] _data = [];
+    private int _count = 0;
+
+    public int Capacity { get { return _data.Length; } set { Array.Resize(ref _data, value); } }
+    /// <summary>
+    /// Индексатор
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns></returns>
+    public T this[int i]
+    {
+        get
+        {
+            if (i >= 0 && i < _count)
+                return _data[i];
+            else
+                throw new ArgumentException($"Індекс {i} неприпустимий. У колекції всього {_count} елементів");
+        }
+        set { _data[i] = value; }
+    }
+
+    public delegate void MethodOnExpanded(GenericEnumerator<T> sender, int newSize);
+    public event MethodOnExpanded? OnExpanded;
+
+    /// <summary>
+    /// Добавляет элемент в коллекцию, возвращает индекс добавленного элемента.
+    /// </summary>
+    /// <param name="element"></param>
+    /// <returns></returns>
+    public int Add(T element)
+    {
+        var new_index = _count;
+        _count++;
+
+        if (Capacity == 0)
+        {
+            OnExpanded?.Invoke(this, 4);
+            Capacity = 4;
+        }
+        else
+        if (Capacity <= _count)
+        {
+            OnExpanded?.Invoke(this, Capacity * 2);
+            Capacity *= 2;
+        }
+        _data[new_index] = element;
+        return new_index;
+    }
+    /// <summary>
+    /// Реализация методов  IEnumerable<T>
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (var i = 0; i < _count; i++)
+        {
+            yield return _data[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Text;
+using HomeWork_4;
+
+
+Console.OutputEncoding = Encoding.UTF8; // Інакше українське "і" не виводилось.
+Console.Title = "Домашка №4";
+
+
+#region Declarations
+var strCollection = new GenericEnumerator<string>();
+// івент метод
+static void MethodOnExpanded(GenericEnumerator<string> sender, int newSize) =>
+                                                                              Console.WriteLine($"  Розширили масив _data  до розміру {newSize}");
+strCollection.OnExpanded += MethodOnExpanded;
+#endregion
+
+#region Ініціалізація
+//strCollection.Capacity = 22;
+var elementCount = 20;
+Console.WriteLine($"Ініціалізація коллекції до {elementCount} элементов");
+for (var i = 0; i < elementCount; i++)
+{
+    strCollection.Add($"Name {elementCount - i}");
+}
+PrintCollection(strCollection, "Коллекція після ініціалізації:");
+#endregion
+
+#region Приклади Linq
+var strSorted = strCollection.OrderBy(x => Convert.ToInt32(x?[5..]));
+PrintCollection(strSorted, "Отсортовано  за зростанням:");
+
+
+var filterMax = 10;
+var strFiltered = strCollection.Where(x => Convert.ToInt32(x?[5..]) > filterMax);
+PrintCollection(strFiltered, $"Отфільтровано по номер більше ніж {filterMax}:");
+#endregion
+
+static void PrintCollection<T>(IEnumerable<T> collection, string msg = "")
+{
+    if (msg.Length > 0)
+        Console.WriteLine(msg);
+    foreach (var obj in collection)
+    {
+        Console.WriteLine($"  {obj?.ToString()}");
+        /*if (Convert.ToInt32(obj?.ToString()[5..]) > filterMax)
+            Console.WriteLine(Convert.ToInt32(obj?.ToString()[5..]));*/
+    }
+    Console.WriteLine();
+}
+
